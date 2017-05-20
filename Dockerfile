30b65c803413d90d3bb42224e8ca7ab80fa7b311
Casey Law <caseyjlaw@gmail.com>
2017-05-19 17:01:47 -0700

    working mock up of sdm read, (fake) gain cal, group, and mean calc per group

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM caseyjlaw/bifrostcpu
 
 RUN apt-get update -y && apt-get install -y emacs ipython python-lxml
 
+ADD bf_sdm.py /bifrost
+
 RUN pip install git+https://github.com/realfastvla/sdmpy.git
 
 # need to install rfpipe
 
 WORKDIR /workspace
-
-ADD bf_sdm.py /bifrost
--- bf_sdm.py
+++ bf_sdm.py
@@ -2,95 +2,62 @@
 # binary_file_io.py
 Basic file I/O blocks for reading and writing data.
 """
+import sys, time, glob
+from datetime import datetime
+from copy import deepcopy
 import numpy as np
-import time
 import bifrost as bf
 import bifrost.pipeline as bfp
 from bifrost.dtype import name_nbit2numpy
 import bifrost.blocks as blocks
 import bifrost.views as views
-
 import sdmpy
-import glob
 
-class BinaryFileWriteBlock(bfp.SinkBlock):
-    """ Write ring data to a binary file
 
-    Args:
-        file_ext (str): Output file extension. Defaults to '.out'
-
-    Notes:
-        output filename is generated from the header 'name' keyword + file_ext
-    """
-
-    def __init__(self, iring, file_ext='out', *args, **kwargs):
-        super(BinaryFileWriteBlock, self).__init__(iring, *args, **kwargs)
-        self.current_fileobj = None
-        self.file_ext = file_ext
-
-    def on_sequence(self, iseq):
-        if self.current_fileobj is not None:
-            self.current_fileobj.close()
-
-        new_filename = iseq.header['name'] + '.' + self.file_ext
-        self.current_fileobj = open(new_filename, 'a')
-
-    def on_data(self, ispan):
-        print "HI"
-        self.current_fileobj.write(ispan.data.tobytes())
-
-class SdmFileRead(object):
-    """ Simple file-like reading object for pipeline testing
+class SdmFileRead(sdmpy.sdm.SDM):
+    """ File-like object for science data model (sdm) files
 
     Args:
-        filename (str): Name of file to open
-        dtype (np.dtype or str): datatype of data, e.g. float32. This should be a *numpy* dtype,
-                                 not a bifrost.ndarray dtype (eg. float32, not f32)
-        gulp_size (int): How much data to read per gulp, (i.e. sub-array size)
+        filename (str): Name of sdm file to open
+        scan_id (int): 1-based index for scan in sdm file
     """
 
     def __init__(self, filename, scan_id):
-        super(SdmFileRead, self).__init__()
-        self.sdm = sdmpy.SDM(filename)
-        #self.gulp_size = gulp_size
+        super(SdmFileRead, self).__init__(filename, scan_id)
         self.scan_id = scan_id
+        self.sdm = sdmpy.SDM(filename)
         self.scan = self.sdm.scan(self.scan_id)
-        self.bdf = self.sdm.scan(self.scan_id).bdf
-
+        
+        self.n_baselines = int(len(self.scan.baselines))
+        self.n_chans     = int(self.scan.numchans[0])
+        self.n_pol       = int(self.scan.bdf.spws[0].npol('cross'))
+        self.n_spw       = int(len(self.scan.spws))
+        self.shape       = (self.n_baselines, self.n_chans*self.n_spw, self.n_pol)
+        print(self.shape)
         self.integration_id = 0
 
     def read(self):
         try:
-            data = self.bdf.get_integration(self.integration_id).get_data(0)
+            data = self.scan.bdf.get_integration(self.integration_id).get_data().reshape(self.shape)
             self.integration_id += 1
-            print self.integration_id
         except IndexError:
             return np.array([])
 
         return data
 
-    def __enter__(self):
-        return self
-
-    def close(self):
-        pass
-
-    def __exit__(self, type, value, tb):
-        self.close()
-
 
 class SdmReadBlock(bfp.SourceBlock):
-    """ Block for reading binary data from file and streaming it into a bifrost pipeline
+    """ Block for reading binary data from sdm file and streaming it into a bifrost pipeline
 
     Args:
-        filenames (list): A list of filenames to open
-        gulp_size (int): Number of elements in a gulp (i.e. sub-array size)
+        filenames (list): A string of filename to open
         gulp_nframe (int): Number of frames in a gulp. (Ask Ben / Miles for good explanation)
-
+        integration_id (int): 0-based index of integration to read
     """
 
-    def __init__(self, filenames, gulp_nframe, integration_id, *args, **kwargs):
-        super(SdmReadBlock, self).__init__(filenames, gulp_nframe, *args, **kwargs)
+    def __init__(self, filename, gulp_nframe, integration_id, *args, **kwargs):
+        super(SdmReadBlock, self).__init__([filename], gulp_nframe, *args, **kwargs)
+        self.filename = filename
         self.integration_id = integration_id
 
     def create_reader(self, filename):
@@ -99,19 +66,21 @@ class SdmReadBlock(bfp.SourceBlock):
 
     def on_sequence(self, ireader, filename):
 
-        n_baselines = int(len(ireader.scan.baselines))
-        n_chans     = int(ireader.scan.numchans[0])
-        n_pol       = 2
-        n_pulse     = 1
+        n_baselines = ireader.n_baselines
+        n_chans     = ireader.n_chans*ireader.n_spw
+        n_pol       = ireader.n_pol
+        shape       = (n_baselines, n_chans, n_pol)
 
-        ohdr = {'name': filename,
+        ohdr = {'name': self.filename,
                 '_tensor': {
                         'dtype':  'cf32',
-                        'shape':  [-1, n_baselines, n_pulse, n_chans, n_pol],
-                        'labels': ['time', 'baseline', 'pulse', 'chan', 'pol']
+                        'shape':  [-1, n_baselines, n_chans, n_pol],
+                        'labels': ['time', 'baseline', 'chan', 'pol']
                         },
-                'gulp_nframe': 1
+                'gulp_nframe': 1,
+                'itershape': shape
                 }
+
         return [ohdr]
 
     def on_data(self, reader, ospans):
@@ -127,23 +96,57 @@ class SdmReadBlock(bfp.SourceBlock):
         else:
             return [0]
 
+
+class GainCalBlock(bfp.TransformBlock):
+    def __init__(self, iring, gainfile, *args, **kwargs):
+        super(GainCalBlock, self).__init__(iring, *args, **kwargs)
+        self.gainfile = gainfile
+
+    def on_sequence(self, iseq):
+        ohdr = deepcopy(iseq.header)
+
+        shape = ohdr['itershape']
+        gr = np.random.normal(size=shape)
+        gi = np.random.normal(size=shape)
+        self.gain = np.zeros(dtype='complex64', shape=shape)
+        self.gain.real = gr
+        self.gain.imag = gi
+        print('Parsed gainfile {0}'.format(self.gainfile))
+
+        return ohdr
+
+    def on_data(self, ispan, ospan):
+        idata = ispan.data
+        ospan.data[...] = idata * self.gain
+
+        return ispan.nframe
+
+
+class PrintMeanBlock(bfp.SinkBlock):
+    def __init__(self, iring, *args, **kwargs):
+        super(PrintMeanBlock, self).__init__(iring, *args, **kwargs)
+        self.n_iter = 0
+
+    def on_sequence(self, iseq):
+        print("Starting sequence processing at [%s]" % datetime.now())
+        self.n_iter = 0
+
+    def on_data(self, ispan):
+        now = datetime.now()
+        print("[%s] %s" % (now, np.mean(ispan.data)))
+        self.n_iter += 1
+
 if __name__ == "__main__":
 
     # Setup pipeline
-    filenames   = sorted(glob.glob('*.cut'))
-    b_read      = SdmReadBlock(filenames, 1, 16)
-    b_copy      = blocks.copy(b_read, space='cuda')
+    filename   = str(sys.argv[1])
+    b_read      = SdmReadBlock(filename, 1, 16)
+    b_cal       = GainCalBlock(b_read, 'test.txt')
     b_scrunched = views.split_axis(
-                    b_copy, axis='time',
-                    n=200, label='fft_window')
-    b_fft       = blocks.fft(
-                    b_scrunched, axes = 'fft_window',
-                    axis_labels='freq')
-    b_out = blocks.copy(b_fft, space='system')
-
-    b_file = BinaryFileWriteBlock(b_out, "output.test")
-
-    blocks.print_header(b_out)
+                    b_cal, axis='time',
+                    n=200, label='window')
+#    b_file = BinaryFileWriteBlock(b_scrunched, "output.test")
+    b_pr = PrintMeanBlock(b_scrunched)
 
     # Run pipeline
     pipeline = bfp.get_default_pipeline()
